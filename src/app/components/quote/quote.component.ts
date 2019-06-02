d0c099f8bb840fd5fdedcbb02cfa7e7251c298b9
Annette Odingo <odingoannette@gmail.com>
2019-06-02 09:28:00 +0300

    Add quote to quote.component.ts

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  qoutes = [
+    new Quote("Annette", "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.","Helen Keller"),
+    new Quote("Annette", "Do not go where the path may lead, go instead where there is no path and leave a trail.","Ralph Waldo Emerson"),
+    new Quote("Annette", "Tell me and I forget. Teach me and I remember. Involve me and I learn.","Benjamin Franklin"),
+    new Quote("Annette", "Spread love everywhere you go. Let no one ever come to you without leaving happier.","Mother Teresa")
+  ]
 
   constructor() { }
 
