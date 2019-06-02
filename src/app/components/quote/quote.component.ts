c083a8d51360c1ebd0199a7b6036a23443b6abf4
Annette Odingo <odingoannette@gmail.com>
2019-06-03 02:30:34 +0300

    Add data to timeCount Pipe

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -7,9 +7,9 @@ import { Quote } from "../.././quote"
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote("Annette", "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.","Helen Keller", new Date(2019,5,2), 0),
-    new Quote("Annette", "Do not go where the path may lead, go instead where there is no path and leave a trail.","Ralph Waldo Emerson", new Date(2019,5,3), 0),
-    new Quote("Annette", "Tell me and I forget. Teach me and I remember. Involve me and I learn.","Benjamin Franklin", new Date(2019,5,2), 0),
+    new Quote("Annette", "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.","Helen Keller", new Date(2019,5,1), 0),
+    new Quote("Annette", "Do not go where the path may lead, go instead where there is no path and leave a trail.","Ralph Waldo Emerson", new Date(2019,5,1), 0),
+    new Quote("Annette", "Tell me and I forget. Teach me and I remember. Involve me and I learn.","Benjamin Franklin", new Date(2019,5,1), 0),
   ]
   addNewQuote(quote){
     this.quotes.unshift(quote);
